aeee88612573eebcd0c06fb299cb4c6ef1ea926c
Neil Empey <argus.two.2@gmail.com>
2020-04-20 05:53:33 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,14 +1,18 @@
-class Cat 
-  attr_accessor :name 
-  
+class Cat
+  attr_writer :name
+  attr_reader :name
+
   def initialize(name)
-    @name = name 
-  end 
-  
+
+    @name = name
+
+  end
+
   def meow
     puts "meow!"
-  end 
-  
-end 
+  end
 
-Toby = Cat.new
+end
+
+ neil = Cat.new("Neil")
+puts neil.name
